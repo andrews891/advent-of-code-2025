62371d5c602d33ac2365cafb5d8f11384f5900d2
andrews891 <andrewsimpson891@gmail.com>
2025-12-22 08:21:56 +0000

    day-06

--- day-06/day-06.py
+++ day-06/day-06.py
@@ -16,21 +16,27 @@ for i, operation in enumerate(operations):
         results[i] = np.prod(numbers[i])
 
 print(f"part 1: {np.sum(results)}")
-results = np.tile(0, operations.shape)
-numbers = np.array([list(line.replace("\n", "")) for line in lines[:-1]]).T
-numbers = np.array(
-    [r.replace(" ", "") for r in ["".join(row) for row in numbers.tolist()]]
-)
-numbers = numbers[np.where(numbers != "")]
-print(numbers)
-p_size = numbers.shape[0] // operations.shape[0]
-numbers = np.reshape(numbers, (-1, p_size))
-numbers = np.array(numbers, dtype=np.int64)
 
-for i, operation in enumerate(operations):
-    if operation == "+":
-        results[i] = np.sum(numbers[i])
-    else:
-        results[i] = np.prod(numbers[i])
+results = []
 
-print(f"part 2: {np.sum(results)}")
+numbers = np.array([[char for char in line.replace("\n", "")] for line in lines])
+
+register = []
+
+for c in range(numbers.shape[1] - 1, -1, -1):
+    num = "".join(numbers[:, c])
+    num, op = num[:-1].strip(), num[-1]
+
+    if num == "":
+        register = []
+        continue
+
+    register.append(num)
+
+    if op == "+":
+        results.append(np.sum(np.array(register, dtype=np.int64)))
+    elif op == "*":
+        results.append(np.prod(np.array(register, dtype=np.int64)))
+
+
+print(f"part 2: {np.sum(np.array(results, dtype=np.int64))}")
